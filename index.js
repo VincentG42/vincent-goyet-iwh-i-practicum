5cc2d7a07debd21a6fdd814c2ffa088db47f8f9c
MacgoyG42 <goyetvincent.dev@gmail.com>
2025-04-08 17:16:03 +0200

    Add functional update-cobj routes and pug template to add record

--- index.js
+++ index.js
@@ -39,11 +39,40 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { 
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    
+    // Création de l'objet avec les données du formulaire
+    const newRecord = {
+        properties: {
+            name: req.body.name,
+            artist: req.body.artist,
+            release_year: req.body.release_year,
+            genre: req.body.genre,
+            condition: req.body.condition,
+            id: req.body.id
+        }
+    };
+    
+    try {
+        console.log('Creating new record:', JSON.stringify(newRecord, null, 2));
+        await axios.post(vinylRecordsEndpoint, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error details:', error.response ? error.response.data : error.message);
+        res.status(500).send('Error creating new vinyl record');
+    }
+});
 
 
 
